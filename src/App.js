e3349f4dd38cad14339ed40ed26cb8890fe73c2c
hsn <systemry420gmail.com>
2020-10-28 20:45:20 +0200

    control the input field

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      text: "",
       data: [
         { name: "aaa", completed: false },
         { name: "bbb", completed: true },
@@ -14,23 +15,42 @@ class App extends React.Component {
     }
   }
 
-  addTodo(e) {
-    alert(e)
+  
+  checkItem() {
+    
+  }
+
+  handleChange = (e)=> {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  
+  
+  addTodo = e => {
+    e.preventDefault()
+    console.log('e', e.target.input.value);
   }
 
   render() {
     return (
       <div className="App">
-        <h1>TODOS</h1>
+        <h1>TODOS {this.state.text}</h1>
         <div className="input">
-          <form onSubmit={()=> this.addTodo}>
-            <input type="text" placeholder="Enter todo item" />
+          <form onSubmit={this.addTodo}>
+            <input 
+              name="input"   //name to access it
+              value={this.state.text}   // value to control it
+              type="text" 
+              placeholder="Enter todo item" 
+              onChange={this.handleChange}   // to handle the change
+            />
             <button type="submit">Add</button>
           </form>
         </div>
         <div className="todos">
           {this.state.data.map((d, i)=>{
-            return <Todo {...d} key={i} />
+            return <Todo check={()=>this.checkItem} {...d} key={i} />
           })}
         </div>
       </div>
--- src/Todo.js
+++ src/Todo.js
@@ -5,7 +5,7 @@ import './App.css';
 function Todo(data) {
     return (
         <div className="todo">
-            <input type="checkbox" name="todos" checked={data.completed} />
+            <input readOnly onChange={data.checkItem} type="checkbox" name="todos" checked={data.completed} />
             <span className="name">{data.name}</span>
             <span className="delete">
                 X
