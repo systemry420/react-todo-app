03c500d287055394991d16273323d2a39f464f0b
hsn <systemry420gmail.com>
2020-10-28 18:37:32 +0200

    add the initial app skeleton with style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React from 'react'
 import './App.css';
+import Todo from './Todo'
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        { name: "aaa", completed: false },
+        { name: "bbb", completed: true },
+        { name: "ccc", completed: false },
+      ]
+    }
+  }
+
+  addTodo(e) {
+    alert(e)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TODOS</h1>
+        <div className="input">
+          <form onSubmit={()=> this.addTodo}>
+            <input type="text" placeholder="Enter todo item" />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <div className="todos">
+          {this.state.data.map((d, i)=>{
+            return <Todo {...d} key={i} />
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+
+function Todo(data) {
+    return (
+        <div className="todo">
+            <input type="checkbox" name="todos" checked={data.completed} />
+            <span className="name">{data.name}</span>
+            <span className="delete">
+                X
+            </span>
+        </div>
+    )
+}
+
+export default Todo
